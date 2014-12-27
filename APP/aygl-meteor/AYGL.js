da58377af115a212b9543b894de74870295594ca
Daryl Tan <daryltanwk@gmail.com>
2014-12-27 22:23:44 +0800

    User Registration Page, Bootstrap additions
    - completed registration page design
    - added in client-side visual validation logic for registration page
    - added in bootstrap.js to handle javascript elements of bootstrap (packaged with bootstrap)
    - added in glyphicon fonts (packaged with bootstrap)

--- APP/aygl-meteor/AYGL.js
+++ APP/aygl-meteor/AYGL.js
@@ -72,6 +72,106 @@ if (Meteor.isClient) {
 
         }
     });
+
+    Template.verifiedsignupform.helpers({
+        validUserState: function() {
+            var result;
+            if (Session.get('usernameValid')===true) {
+                result = {
+                    form: "has-success has-feedback",
+                    logo: "glyphicon-ok"
+                };
+                return result;
+            } else if(Session.get('usernameValid')===false){
+                result = {
+                    form: "has-error has-feedback",
+                    logo: "glyphicon-remove"
+                };
+                return result;
+            } else {
+              result = {
+                form:" ",
+                logo:" "
+              };
+              return result;
+            }
+        },
+        validPassState: function() {
+            var result;
+            if (Session.get('passwordValid')) {
+                result = {
+                    form: " ",
+                    logo: " "
+                };
+                $(function() {
+                    $("#password1").popover('hide');
+                });
+                return result;
+            } else {
+                result = {
+                    form: "has-warning has-feedback",
+                    logo: "glyphicon-warning-sign"
+                };
+                $(function() {
+                    $("#password1").popover('show');
+                });
+                return result;
+            }
+        },
+        usernameCharLeft: function() {
+          return Session.get('usernameCharLeft');
+        }
+    });
+
+    Template.verifiedsignupform.rendered = function() {
+        $(function() {
+            $("#username,#password,#email").popover({
+                placement: 'right',
+                trigger: 'focus',
+                container: 'body'
+            });
+        });
+        $(function() {
+            $("#password1").popover({
+                placement: 'right',
+                trigger: 'manual',
+                container: 'body'
+            });
+        });
+        Session.set('usernameCharLeft', 25);
+        Session.set('passwordValid', true);
+    };
+
+    Template.verifiedsignupform.events({
+        'keyup #username, blur #username': function() {
+            //CHECK USERNAME INPUT
+            var regexPattern = /^[a-z0-9_-]+$/i;
+            var name = Template.instance().find('#username').value;
+            var result = regexPattern.test(name);
+            Session.set('usernameValid', result);
+
+            //CHECK USERNAME COUNT
+            var usernameCharLimit = 25;
+            if (name.length > usernameCharLimit) {
+                Template.instance().find('#username').value = name.substr(0, usernameCharLimit);
+            }
+            Session.set('usernameCharLeft', usernameCharLimit - Template.instance().find('#username').value.length);
+        },
+        'keyup #password1, blur #password1, keyup #password, blur #password': function() {
+            //CHECK PASSWORD WITH PASSWORD1 FIELD
+            var pass1 = Template.instance().find('#password1').value;
+            var pass = Template.instance().find('#password').value;
+
+            if (pass === "" || pass1 === "") {
+                Session.set('passwordValid', true);
+            }else if (pass1 !== pass) {
+                Session.set('passwordValid', false);
+            } else {
+                Session.set('passwordValid', true);
+            }
+        }
+    });
+
     Template.verifiedsignupinfo.helpers({
         idty: function() {
             return VerifyTab.findOne();
