0cc40327a54e4bbd40b40de28084f62e8b26e081
Daryl Tan <daryltanwk@gmail.com>
2014-12-27 10:19:07 +0800

    Verified Signup updates/ Logo enhancements
    - verified signup still incomplete, but steam api handling side is basically complete and secure (i think!)
    - changed some logos for better viewing on the web
    - added a loading page (may design page to be the default loading page for all parts of this site, but currently in hard-code)
    
    TODO:
    - design the registration page
    - add in user account creation process
    - design a landing page for logged in users
    
    NOTES:
    - signup process so far
    1. send user to steam
    2. steam redirects back to server
    3. handle steam redirect automatically, send details to steam for double confirmation of details received
    4. handle steam response on details verification and redirect user to pass/fail page

--- APP/aygl-meteor/AYGL.js
+++ APP/aygl-meteor/AYGL.js
@@ -1,25 +1,58 @@
-Router.configure({
-    layoutTemplate: 'MainLayout'
-});
+VerifyTab = new Mongo.Collection('vtab');
 
 Router.route('/', function() {
     this.render('main');
 });
+Router.route('/verifiedsignup/:sig', function() {
+    var sig = encodeURI(this.params.sig);
+    var routerObj = this;
+    routerObj.render('loading');
+    Meteor.call('checkValidSignup',sig, function(err, res) {
+        if(!res) {
+          //NOPES. Please Try again
+        } else{
+          //RENDER REGISTRATION PAGE
+          routerObj.render('loading');
+        }
+    });
 
-Router.route('/signin', function() {
-    newlink = 'https://steamcommunity.com/openid/login';
-    newlink = newlink + window.location.search;
-    newlink = newlink.replace("id_res", "check_authentication");
-    window.location.replace(newlink);
-    this.render('main');
 });
 
+Router.route('/signin', function() {
+    var newparams = this.params.query;
+    VerifyTab.insert({
+        steamID: newparams['openid.claimed_id'].substring(36, newparams['openid.claimed_id'].length),
+        sig: encodeURI(newparams['openid.sig'])
+    });
+    var newurl = 'https://steamcommunity.com/openid/login';
+    var rinstance = this;
+    var newlink = newurl + this.originalUrl;
+    newlink = newlink.replace('id_res', 'check_authentication');
+    HTTP.call('POST', newlink, function(err, res) {
+        if (res.content.search('is_valid:true') !== -1) {
+            //IS VALID
+            rinstance.response.writeHead(301, {
+                'Location': '/verifiedsignup/' + newparams['openid.sig']
+            });
+            rinstance.response.end();
+        } else {
+            //IS INVALID. display evil messages for the bad boys.
+            rinstance.response.end('bad boys is bad. die bad boys!');
+        }
+    });
+}, {
+    where: 'server'
+});
+
+
+
 if (Meteor.isClient) {
+    Meteor.subscribe('lol');
     Template.main.helpers({
         steamloginlink: function() {
             var url = 'https://steamcommunity.com/openid/login?openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select&openid.identity=http://specs.openid.net/auth/2.0/identifier_select&openid.mode=checkid_setup&openid.ns=http://specs.openid.net/auth/2.0&openid.realm=http://localhost:3000/&openid.return_to=http://localhost:3000/signin/';
 
-                return url;
+            return url;
 
         }
     });
@@ -28,5 +61,22 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
     Meteor.startup(function() {
         // code to run on server at startup
+        Meteor.publish('lol', function() {
+            return VerifyTab.find();
+        });
+    });
+
+    Meteor.methods({
+        checkValidSignup: function(siggy) {
+            if (VerifyTab.findOne({
+                    sig: siggy
+                }) === undefined) {
+                return false;
+            } else {
+                return VerifyTab.findOne({
+                    sig: siggy
+                })['steamID'];
+            }
+        }
     });
 }
