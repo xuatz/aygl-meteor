efec4bf1cb64e37988f6802dec7a179513309886
xuatz <xzlow10@gmail.com>
2015-01-11 19:24:31 +0800

    Update AYGL.js
    end of adventure

--- APP/aygl-meteor/AYGL.js
+++ APP/aygl-meteor/AYGL.js
@@ -1,7 +1,6 @@
 VerifyTab = new Mongo.Collection('vtab');
 le_web_address = '128.199.86.69:3001';
 
-
 if (Meteor.isClient) {
     Template.mainregister.helpers({
         steamloginlink: function() {
