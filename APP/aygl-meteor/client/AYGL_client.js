75400ac5fc4af070037dff220de4d1274d293bcb
xuatz <xzlow10@gmail.com>
2015-02-21 23:14:58 +0800

    Revert "no message"
    This reverts commit 66360aef3dd4af9a72348bae1a26ba94d00b059e.

--- APP/aygl-meteor/client/AYGL_client.js
+++ APP/aygl-meteor/client/AYGL_client.js
@@ -1,11 +1,3 @@
-
-//Common Strings
-
-DOTA_MATCH_ID_COLON = "Dota Match ID:"
-
-//============
-
-
 Template.mainregister.helpers({
     steamloginlink: function() {
         var url = 'https://steamcommunity.com/openid/login?openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select&openid.identity=http://specs.openid.net/auth/2.0/identifier_select&openid.mode=checkid_setup&openid.ns=http://specs.openid.net/auth/2.0&openid.realm=http://' + le_web_address + '/&openid.return_to=http://' + le_web_address + '/signin/';
