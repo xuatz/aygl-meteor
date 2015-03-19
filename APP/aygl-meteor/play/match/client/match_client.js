31cd48e7c4df0897b7c29554a4b26a5a1143491f
xuatz <xzlow10@gmail.com>
2015-03-19 17:55:57 +0800

    dynamic detection and toggling of thumbs up and down (working)

--- APP/aygl-meteor/play/match/client/match_client.js
+++ APP/aygl-meteor/play/match/client/match_client.js
@@ -1,88 +1,121 @@
 Meteor.subscribe('MatchesCollection');
 
 Template.matchradiant.helpers({
-    demo18: function() {
-        return {'username': 'huatah'};
-    },
-    demo2000: function() {
-        var m = MatchesCollection.findOne();
-
-        console.log(m);
-        console.log(m.result);
-
-        return m;
-    },
-    getPlayerDetails: function(playerSlot) {
-        var data = Meteor.call('demoMatchDetailsMethod', function(err, res) {
-            if (!err) {
-                var list = res.matchPlayerResultArr;
-                var item = list[playerSlot];
-
-                // console.log(res);
-                // console.log(list);
-                // console.log(item);
-                
-                console.log(item.username);
-
-                return item;
-            }
-        });
-    },
     getRadPlayers: function() {
         var data = MatchesCollection.findOne();
 
-        var radPlayers = _.filter(data.matchPlayerResultArr, 
-            function(player_slot) { 
-                return player_slot.player_slot < 5; 
+        console.log('===================');
+        console.log(data);
+
+        var radPlayers = _.filter(data.matchPlayerResults, 
+            function(item) { 
+                return item.playerSlot < 5; 
             }
         );
 
-        var radPlayers = _.sortBy(radPlayers, function(item)
-            { return Math.sin(item.player_slot); }
-        );
+        var radPlayers = _.sortBy(radPlayers, function(item) { 
+            return item.playerSlot; 
+        });
 
-        console.log('radPlayers: ' + radPlayers);
-
-        console.log('radPlayers: ' + radPlayers[0].username);
-
-        return radPlayers;
-    },
-    getDirePlayers: function() {
-        var data = MatchesCollection.findOne();
-
-        console.log(data.matchPlayerResultArr);
-
-        var radPlayers = _.filter(data.matchPlayerResultArr, 
-            function(player_slot) { 
-                return player_slot.player_slot > 4; 
-            }
-        );
-
-        var radPlayers = _.sortBy(radPlayers, function(item)
-            { return Math.sin(item.player_slot); }
-        );
-
-        console.log(radPlayers);
+        // console.log('radPlayers: ' + radPlayers);
+        // console.log('radPlayers: ' + radPlayers[0].username);
 
         return radPlayers;
     }
 });
 
+Template.matchdire.helpers({
+    getDirePlayers: function() {
+        var data = MatchesCollection.findOne();
 
+        console.log('===== getDirePlayers!! ==============');
+        console.log(data);
 
-Template.demo1.events({
-    'click #huatah' : function(event) {
-        console.log('its me, huatah');
-
-        bootbox.prompt({
-            title: "Comments for " + this.name,
-            callback: function(result) {
-                if (result === null) {
-                    console.log('he left nothing');
-                } else {
-                    console.log('are you sure?');
-                }
+        var players = _.filter(data.matchPlayerResults, 
+            function(item) { 
+                return item.playerSlot > 4; 
             }
+        );
+
+        var players = _.sortBy(players, function(item) { 
+            return item.playerSlot;
         });
+
+        return players;
+    }
+});
+
+
+
+Template.playerPanel.events({
+    'click #thumbsUp' : function(event) {
+        event.preventDefault();
+        var something = $(event.target);
+
+        console.log(something);
+
+        if (something.hasClass( "text-success" ) ) {
+            something.removeClass('text-success');
+            //TODO
+            //user.commend--;
+        } else {
+            something.addClass('text-success');
+            //TODO
+            //user.commend++;
+            var siblingThumbsDown = $(event.target).next('i');
+            if (siblingThumbsDown.hasClass("text-danger") ) {
+                siblingThumbsDown.removeClass('text-danger');
+                //TODO
+                //user.report--;
+            }
+        }
+
+        // bootbox.prompt({
+        //     title: this.username + " best player ever. Rated 10/10",
+        //     callback: function(result) {
+        //         if (result === null) {
+        //             console.log('he left nothing');
+        //         } else {
+        //             console.log('are you sure?');
+        //             //TODO save comments to commend
+        //         }
+        //     }
+        // });
+    },
+    'click #thumbsDown' : function(event) {
+        event.preventDefault();
+        var something = $(event.target);
+
+        console.log(something);
+        console.log('=================');
+
+        if (something.hasClass( "text-danger" ) ) {
+            something.removeClass('text-danger');
+            //TODO
+            //user.report--;
+        } else {
+            something.addClass('text-danger');
+            //TODO
+            //user.report++;
+            
+            var previousThumbsUp = $(event.target).prev('i');
+            if (previousThumbsUp.hasClass("text-success") ) {
+                previousThumbsUp.removeClass('text-success');
+                //TODO
+                //user.commend--;
+            }
+        }
+
+        // bootbox.prompt({
+        //     title: "omg " + this.username +  ", S A D B O Y S",
+        //     callback: function(result) {
+        //         if (result === null) {
+        //             console.log('he left nothing');
+        //         } else {
+        //             console.log('are you sure?');
+        //             //TODO save comments to commend
+        //         }
+        //     }
+        // });
     }
 });
