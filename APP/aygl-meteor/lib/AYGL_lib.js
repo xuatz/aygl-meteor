0ce4f923eaa5c16f23af0ded3e40a66dc9ad881d
xuatz <lowxuanzhi@gmail.com>
2015-03-31 14:45:00 +0800

    added update and insert hooks for Games, Alerts and MatchesCollection (cant get the default hooks options to work, sad)

--- APP/aygl-meteor/lib/AYGL_lib.js
+++ APP/aygl-meteor/lib/AYGL_lib.js
@@ -22,6 +22,39 @@ MatchesCollection = new Mongo.Collection("Match");
 
 MatchesCollection.allow({
     // insert:function(){return true;},
-    remove:function(){return true;},
+    // remove:function(){return true;},
     update:function(){return true;},
 });
+
+//=================================
+
+var beforeInsertOptions = function(username, doc) {
+  //console.log('hi!! im in before insert!');
+  doc.createdDttm = Date.now();
+  doc.createdBy = username;
+};
+
+var beforeUpdateOptions = function(username, doc, fieldNames, modifier, options){
+  // console.log('doc');
+  // console.log(doc);
+  // console.log('fieldNames');
+  // console.log(fieldNames);
+  // console.log('modifier');
+  // console.log(modifier);
+
+  modifier.$set = modifier.$set || {};
+  modifier.$set.updatedDttm = Date.now();
+  modifier.$set.updatedBy = username;
+};
+
+//=================================
+
+//XZ: i cant get the default.options to work =(
+
+Games.before.insert(beforeInsertOptions);
+Alerts.before.insert(beforeInsertOptions);
+MatchesCollection.before.insert(beforeInsertOptions);
+
+Games.before.update(beforeUpdateOptions);
+Alerts.before.update(beforeUpdateOptions);
+MatchesCollection.before.update(beforeUpdateOptions);
