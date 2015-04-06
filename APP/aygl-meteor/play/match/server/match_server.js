c6d796832e216ca5deb15e6f2188ac63ed30d914
xuatz <xzlow10@gmail.com>
2015-04-06 18:29:10 +0800

    made "sendMatchDetailsToMainDB" a global meteor method because admin module is reusing it

--- APP/aygl-meteor/play/match/server/match_server.js
+++ APP/aygl-meteor/play/match/server/match_server.js
@@ -1,30 +1,114 @@
-var crypto = Npm.require('crypto');
+var crypto = Npm.require('crypto')
+	,Future = Npm.require('fibers/future');
 
-function checkIfCptReportedScore(g, side) {
-	switch (side) {
-		case 'R':
-			var item = _.find(g.scoreReports, function(item){
-				if (item !== null) {
-					return item.playerSlot == 0;
-				}
-			});
-			break;
-		case 'D':
-			var item = _.find(g.scoreReports, function(item){
-				if (item !== null) {
-					return item.playerSlot == 5;
-				}
-			});
-			break;
-	}
+Meteor.methods({
+	updatePlayerScoreReport: function(gameId, username, playerSlot, result) {
+		console.log('check env var');
+        console.log(process.env.HASH_SALT);
 
-	if (item) {
-		//console.log(item);
-		return true;
-	} else {
-		return false;
+		//TODO demo for dev purpose only
+        var g = Games.findOne({});
+        var gameId = g._id;
+
+				//================
+
+        //we need to upsert a the match report into game item for current player (client)
+        var fieldString = 'scoreReports.' + playerSlot;
+
+        var obj = {};
+        obj[fieldString] =
+        {
+            'username': username,
+            'playerSlot': playerSlot,
+            'result': result
+        };
+
+        Games.upsert(
+            { _id : gameId },
+            {
+                $set: obj
+            }
+        );
+
+		checkMatchResultReports(gameId);
+	},
+	sendMatchDetailsToMainDB: function(matchDetailsId) {
+		console.log('sendMatchDetailsToMainDB');
+
+		//hardcoded for dev
+		var matchDetails = MatchesCollection.findOne();
+		// var matchDetails = MatchesCollection.findOne({
+		// 	_id : matchDetailsId
+		// });
+
+		var matchString = JSON.stringify(matchDetails);
+		//var matchString = JSON.stringify(matchDetails);
+
+		var password = '/match' + matchString;
+		// console.log('==================');
+		// console.log('password');
+		// console.log(password);
+
+		var salt = process.env.HASH_SALT;
+		var iterations = parseInt(process.env.HASH_ITERATIONS);
+		var keylen = parseInt(process.env.HASH_KEYLEN);
+
+	    var hash = crypto.pbkdf2Sync(password, salt, iterations, keylen).toString('base64');
+
+	    // console.log('==================');
+	    // console.log(hash);
+	    
+	    var fut = new Future();
+
+	    HTTP.call("POST", "http://localhost:3000/match",
+	        {
+	            headers: {
+	                authorization: "aygldb " + hash
+	            }
+	            , params: {matchDetails: matchString}
+	        }, function(err, res) {
+	        	if (err) {
+	        		console.log('==================');
+	        		console.log('there is an error');
+	        		console.log(err);
+
+	        		//TODO log error somewhere???
+
+	        		fut.throw(err);
+	        	}
+	        	if (res) {
+	        		console.log('==================');
+	        		console.log('there is an res');
+	        		console.log(res);
+	        		console.log('==================');
+	        		console.log(res.content);	        		
+
+	        		if (res.statusCode === 201) {
+	        			//its a success!
+	        			//TODO delete corresponding matchDetails and Game
+	                } else {
+	                	//if fail
+	                	MatchesCollection.update(
+							{
+								_id : matchDetails._id
+							},
+							{
+								status : 'PU'
+							}
+						);
+	                	//TODO log error somewhere???
+	                }
+
+	                fut.return(res.statusCode);
+	            }
+	        }
+	    );
+
+	    return fut.wait();
 	}
-};
+});
+
+//======================================
 
 var checkMatchResultReports = function(gameId) {
 	console.log('Start of checkMatchResultReports');
@@ -38,12 +122,6 @@ var checkMatchResultReports = function(gameId) {
 		if (!g.scoreReports) {
 			g.scoreReports = [];
 		}
-
-		// g.scoreReports[0] = {
-		// 	'username': 'moltencrap',
-		// 	'playerSlot': '0',
-		// 	'result': 'V'
-		// };
 
 		console.log(g);
 		console.log('==========');
@@ -106,64 +184,71 @@ var checkMatchResultReports = function(gameId) {
 				var m = MatchesCollection.findOne({'gameId': g._id});
 				//we fetch latest before taking action cos maybe some1 else's action already resolved this game
 
-				if (m.result) {
-					//since the matchDetails already have a result liao, dun need to do anything liao
+				if (!m) {
+					console.log('There is a big problem, why is there no matchDetails?');
 				} else {
-					var cptRadScoreReport = _.find(g.scoreReports, function(item){
-												if (item) {
-													return item.playerSlot == 0;
-												}
-											});
-					var cptDireScoreReport = _.find(g.scoreReports, function(item){
-												if (item) {
-													return item.playerSlot == 5;
-												}
-											});
-
-					console.log('cptRadReportedScore: ' + cptRadScoreReport);
-					console.log('cptDireReportedScore: ' + cptDireScoreReport);
-
-					if (cptRadScoreReport && cptDireScoreReport) {
-						if (cptRadScoreReport.result == cptDireScoreReport.result) {
-
-							var reasonableTimeElapsedSinceMatchLobbyCreated = true;
-							var matchDetailsCreatedDttm = moment(matchDetails.createdDttm);
-							var durationSinceCreation = moment.duration(moment().diff(matchDetailsCreatedDttm));
-
-							if (duration.minutes() > 10) {
-								reasonableTimeElapsedSinceMatchLobbyCreated = true;
-							} else {
-								reasonableTimeElapsedSinceMatchLobbyCreated = false;
-							}
-
-							takeActionOnMatchDetailsBasedOnResult(m, cptRadScoreReport.result, reasonableTimeElapsedSinceMatchLobbyCreated);
-						} else {
-							updateMatchDetailsResultAsInvestigation(m);
-						}
+					if (m.result) {
+						//since the matchDetails already have a result liao, dun need to do anything liao
 					} else {
-						//The point is that, we will in general, use the cpt report as absolute
-						//hence we give a 3 hours grace peroid starting from first match report timing
+						var cptRadScoreReport = _.find(g.scoreReports, function(item){
+													if (item) {
+														return item.playerSlot == 0;
+													}
+												});
+						var cptDireScoreReport = _.find(g.scoreReports, function(item){
+													if (item) {
+														return item.playerSlot == 5;
+													}
+												});
 
-						console.log('scoreReportedDttm: ' + g.matchScoreReportedDttm);
+						console.log('cptRadReportedScore: ' + cptRadScoreReport);
+						console.log('cptDireReportedScore: ' + cptDireScoreReport);
 
-						var reportDttm = moment(g.matchScoreReportedDttm);
-						var duration = moment.duration(moment().diff(reportDttm));
+						if (cptRadScoreReport && cptDireScoreReport) {
+							if (cptRadScoreReport.result == cptDireScoreReport.result) {
 
-						//console.log(duration);
-						console.log(duration.minutes());
-						console.log(duration.hours());
+								var reasonableTimeElapsedSinceMatchLobbyCreated = true;
+								var matchDetailsCreatedDttm = moment(matchDetails.createdDttm);
+								var durationSinceCreation = moment.duration(moment().diff(matchDetailsCreatedDttm));
 
-						//TODO section Z
-						if (duration.hours() >= 3) {
-							//after 3 hours, we will take the party member reports into consideration
-
-							if (g.scoreReports.size()>0) {
-								var topResult = getMostPopularResult(g);
-
-								if (topResult) {
-									takeActionOnMatchDetailsBasedOnResult(matchDetails, topResult, true);
+								if (duration.minutes() > 10) {
+									reasonableTimeElapsedSinceMatchLobbyCreated = true;
 								} else {
-									updateMatchDetailsResultAsInvestigation(m);
+									reasonableTimeElapsedSinceMatchLobbyCreated = false;
+								}
+
+								m.result = cptRadScoreReport.result;
+								takeActionOnMatchDetailsBasedOnResult(m, reasonableTimeElapsedSinceMatchLobbyCreated);
+
+							} else {
+								updateMatchDetailsResultAsInvestigation(m);
+							}
+						} else {
+							//The point is that, we will in general, use the cpt report as absolute
+							//hence we give a 3 hours grace peroid starting from first match report timing
+
+							console.log('scoreReportedDttm: ' + g.matchScoreReportedDttm);
+
+							var reportDttm = moment(g.matchScoreReportedDttm);
+							var duration = moment.duration(moment().diff(reportDttm));
+
+							//console.log(duration);
+							console.log(duration.minutes());
+							console.log(duration.hours());
+
+							//TODO section Z
+							if (duration.hours() >= 3) {
+								//after 3 hours, we will take the party member reports into consideration
+
+								if (g.scoreReports.size()>0) {
+									var topResult = getMostPopularResult(g);
+
+									if (topResult) {
+										matchDetails.result = topResult;
+										takeActionOnMatchDetailsBasedOnResult(matchDetails, true);
+									} else {
+										updateMatchDetailsResultAsInvestigation(m);
+									}
 								}
 							}
 						}
@@ -171,10 +256,7 @@ var checkMatchResultReports = function(gameId) {
 				}
 			}
 		}
-
-		//console.log('Still waiting for more score reports from players...');
 	}
-	sendMatchDetailsToBanana();
 }
 
 var getMostPopularResult = function(game) {
@@ -220,36 +302,6 @@ var getMostPopularResult = function(game) {
 	}
 }
 
-Meteor.methods({
-	updatePlayerScoreReport: function(gameId, username, playerSlot, result) {
-				//TODO demo for dev purpose only
-        var g = Games.findOne({});
-        var gameId = g._id;
-
-				//================
-
-        //we need to upsert a the match report into game item for current player (client)
-        var fieldString = 'scoreReports.' + playerSlot;
-
-        var obj = {};
-        obj[fieldString] =
-        {
-            'username': username,
-            'playerSlot': playerSlot,
-            'result': result
-        };
-
-        Games.upsert(
-            { _id : gameId },
-            {
-                $set: obj
-            }
-        );
-
-				checkMatchResultReports(gameId);
-	}
-});
-
 var updateMatchDetailsResultAsInvestigation = function(matchDetails) {
 	console.log('Match pending investigation!');
 	//TODO XZ: implement CommonConstants
@@ -258,66 +310,37 @@ var updateMatchDetailsResultAsInvestigation = function(matchDetails) {
 			_id : matchDetails._id
 		},
 		{
-			result : 'I'
+			status : 'PI',
+			result : matchDetails.result
 		}
 	);
 }
 
-var takeActionOnMatchDetailsBasedOnResult = function(matchDetails, result, reasonableTimeElapsedSinceMatchLobbyCreated) {
-	switch (result) {
+var takeActionOnMatchDetailsBasedOnResult = function(matchDetails, reasonableTimeElapsedSinceMatchLobbyCreated) {
+	//TODO XZ: implement CommonConstants
+	switch (matchDetails.result) {
 		case 'V':
-			//TODO delete match
+			//TODO delete match and game
 			console.log('Match deleted(Void)');
 			break;
 		case 'R':
 		case 'D':
 			if (reasonableTimeElapsedSinceMatchLobbyCreated) {
-				//TODO MatchDetails.result = cptRadScoreReport.result
-				//TODO send matchDetails to banana!!!
-				console.log('Match processing!');
+				MatchesCollection.update(
+					{
+						_id : matchDetails._id
+					},
+					{
+						status : 'PU',
+						result : matchDetails.result
+					}
+				);
+				Meteor.call('sendMatchDetailsToMainDB', matchDetails._id);
 			} else {
-				//TODO set result as I
 				//potentially some1 is abusing the system
 				//i.e. both cpt just spam DIRE win without any games played to boost result
-				matchDetails.result = 'I';
-				console.log('Match pending investigation!');
+				updateMatchDetailsResultAsInvestigation(matchDetails);
 			}
 			break;
 	}
-}
-
-var sendMatchDetailsToBanana = function() {
-	console.log('demo send stuff to banana');
-
-	var match = MatchesCollection.findOne();
-	var matchString = JSON.stringify(match);
-
-	var password = '/match' + matchString;
-	// console.log('password');
-	// console.log(password);
-
-    var salt = "byvGX7KLa4";
-    var iterations = 2;
-    var keylen = 128;
-
-    var hash = crypto.pbkdf2Sync(password, salt, iterations, keylen).toString('base64');
-
-    // console.log(hash);
-    // console.log('==================');
-
-    HTTP.call("POST", "http://localhost:3000/match",
-        {
-            headers: {
-                authorization: "aygldb " + hash
-            }
-            , params: {matchDetails: matchString}
-        }, function(err, res) {
-            if (res) {
-                // console.log('huatah');
-                // console.log('==================');
-                // console.log(res);
-                return res;
-            }
-        }
-    );
 }
